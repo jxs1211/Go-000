8ea909501903badd18d0bc9ab95d36ffaf8333b4
shenxianjie <327411586@qq.com>
2020-12-05 18:52:07 +0800

    week02 homework

---
+++ Week02/how to handle sql_no_row error.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"log"
+)
+
+func main() {
+	service()
+}
+
+/*如果有异常，在此统一处理*/
+func service() {
+	if err := biz(); err != nil {
+		//fmt.Println("err found: %v", err)
+		log.Fatal("err found: ", err)
+		return
+	}
+	fmt.Println("service ok")
+}
+
+/*透传异常*/
+func biz() error {
+	return dao()
+}
+
+/*捕获底层异常并封装在wrap，向上传递*/
+func dao() error {
+	//return nil
+	return errors.Wrap(errors.New("sqlNoRows"), "test")
+}
